2ed21aa308be0e8606b66c87bac537fd6b9f4a74
Lewis Deng <kekuer@gmail.com>
2016-05-31 16:16:17 +0800

    new architecture

--- test.js
+++ test.js
@@ -1,26 +1,49 @@
 var express = require('express');
 var app = express();
-var uploader = require('raw-uploader');
+var Uploader = require('./uploader');
+var SizeFilter = require('./filters/size');
+var MimeFilter = require('./filters/mime');
+var BufferOutput = require('./outputs/buffer');
+var FileOutput = require('./outputs/file');
 var http = require('http');
 
 app.post('/', function (req, res) {
-    uploader({
-        contentTypes: ['image/png', 'text/plain'],
-        minFileSize: 100,
-        maxFileSize: 1000000000,
-        outputType: 'file',
-        destFolder: './abc',
-        createDestFolder: true,
-        disconnectOnErr: true,
-        fileNameHeader: 'file-name',
-        destFileName: 'test',
-        overwrite: true,
-        deepCheckMime: true
-    }, req, res, function (code, data) {
-        console.log(code, data);
-        res.status(code);
-        res.end();
+    const uploader = new Uploader(req);
+    uploader.filters = [
+        new SizeFilter({
+            min: 100,
+            max: 1000000000
+        }),
+        new MimeFilter({
+            types: [
+                'image/jpeg'
+            ]
+        })
+    ];
+
+    uploader.outputs = [
+        new FileOutput({
+            destFolder: './abc',
+            fileName: 'abcd.jpg',
+            overwrite: true
+        }),
+        new BufferOutput({})
+    ];
+
+    uploader.on('error', (err) => {
+        console.log('error:', err);
+    }).on('filtered', (code, pos) => {
+        console.log('filtered:', code, 'pos:', pos);
+        res.sendStatus(code);
+        res.destroy();
+    }).on('done', (label, data) => {
+        console.log(label, data);
+        if (label == 'file') {
+            res.sendStatus(200);
+        }
     });
+
+    uploader.upload();
 });
 
 http.createServer(app).listen(4000);
@@ -28,3 +51,4 @@ http.createServer(app).listen(4000);
 
 // for test
 // curl -v -X POST http://127.0.0.1:4000 -H "Content-Type:image/jpeg" --data-binary @test.jpg
+// curl -v -X POST http://127.0.0.1:4000 -H "Content-Type:image/jpeg" -H "Content-Length:1000" --data-binary @test.jpg
